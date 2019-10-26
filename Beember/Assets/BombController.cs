fe017ffd9d0284bab44664ce16036f8b844f5440
ZorinIvanA <zorin.ivan@outlook.com>
2019-10-26 21:38:47 +0300

    бомбер

---
+++ Beember/Assets/BombController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public GameObject ExplosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        var size = transform.lossyScale;
+        if (transform.position.y - size.y <= -5)
+        {
+            var rigidBody = GetComponent<Rigidbody2D>();
+            rigidBody.gravityScale = 0;
+            rigidBody.velocity = new Vector2(0, 0);
+            Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
