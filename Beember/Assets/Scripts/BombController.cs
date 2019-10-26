91a028e6df8c4c0064449e8c3cb263a7cf2316ec
ZorinIvanA <zorin.ivan@outlook.com>
2019-10-26 22:55:34 +0300

    Геймплей

--- Beember/Assets/Scripts/BombController.cs
+++ Beember/Assets/Scripts/BombController.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class BombController : MonoBehaviour
 {
     public GameObject ExplosionPrefab;
-
-    // Start is called before the first frame update
+    private Vector3 screenBounds;
+    private bool _isAlreadyBombed;
     void Start()
     {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void LateUpdate()
     {
         var size = transform.lossyScale;
-        if (transform.position.y - size.y <= -5)
+        if (transform.position.y - size.y <= -screenBounds.y)
         {
             var rigidBody = GetComponent<Rigidbody2D>();
             rigidBody.gravityScale = 0;
             rigidBody.velocity = new Vector2(0, 0);
             Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!_isAlreadyBombed)
+        {
+            _isAlreadyBombed = true;
+            Instantiate(ExplosionPrefab, collision.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
         }
     }
 }
---
+++ Beember/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,72 @@
+﻿using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using System;
+
+public class PlaneController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Vector2 planeSpeed;
+    private Vector2 screenBounds;
+    private Camera camera;
+    private bool _isBombFalling;
+    private const string BOMB_AXIS_NAME = "Submit";
+    public GameObject BombPrefab;
+    private GameObject _bombInstantinated;
+    public GameObject BuildingPrefab;
+    private GameObject[][] Buildings;
+
+    private const int MAX_BUILDING_HEIGHT = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        planeSpeed = new Vector2(0.05f, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        var buildingsCount = (int) screenBounds.x * 2;
+        print(buildingsCount);
+        var buildingHeight = new System.Random(DateTime.Now.Millisecond);
+        Buildings = new GameObject[buildingsCount][];
+        for (int i = 0; i < buildingsCount; i++)
+        {
+            var height = buildingHeight.Next(MAX_BUILDING_HEIGHT);
+            Buildings[i] = new GameObject[height];
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 position = new Vector2(i - (int)screenBounds.x + 0.5f, j - screenBounds.y + 0.5f);
+                Buildings[i][j] = Instantiate(BuildingPrefab, position, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = rigidbody.position + planeSpeed;
+        rigidbody.MovePosition(newPosition);
+
+        float bombTrhow = Input.GetAxis(BOMB_AXIS_NAME);
+        if (bombTrhow != 0 && !_isBombFalling)
+        {
+            var size = transform.lossyScale;
+            _isBombFalling = true;
+            Vector3 bombCoords = new Vector3(transform.position.x + size.x / 2,
+                transform.position.y - size.y / 2 - 0.1f, transform.position.z);
+            _bombInstantinated = Instantiate(BombPrefab, bombCoords, transform.rotation);
+        }
+
+        if (_bombInstantinated == null)
+            _isBombFalling = false;
+    }
+
+    void LateUpdate()
+    {
+        var newPosition = transform.position;
+        if (newPosition.x >= screenBounds.x + 1.5f)
+        {
+            newPosition.x = -7f;
+            newPosition.y -= 1;
+        }
+        transform.position = newPosition;
+    }
+}
